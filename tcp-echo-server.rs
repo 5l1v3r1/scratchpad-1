3471422cb1c8461301785d37b0973c11e47d57f2
Andrei Vacariu <andrei@avacariu.me>
2015-02-13 15:06:09 -0800

    Add a TCP echo server in Rust

---
+++ tcp-echo-server.rs
@@ -0,0 +1,44 @@
+use std::net::{TcpListener, TcpStream};
+use std::io::ErrorKind;
+use std::thread::Thread;
+
+// traits
+use std::io::Read;
+use std::io::Write;
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buf;
+    loop {
+        // clear out the buffer so we don't send garbage
+        buf = [0; 512];
+        let _ = match stream.read(&mut buf) {
+            Err(e) => panic!("Got an error: {}", e),
+            Ok(m) => {
+                if m == 0 {
+                    // we've got an EOF
+                    break;
+                }
+                m
+            },
+        };
+
+        match stream.write(&buf) {
+            Err(_) => break,
+            Ok(_) => continue,
+        }
+    }
+}
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8888").unwrap();
+    for stream in listener.incoming() {
+        match stream {
+            Err(e) => { println!("failed: {}", e) }
+            Ok(stream) => {
+                Thread::spawn(move || {
+                    handle_client(stream)
+                });
+            }
+        }
+    }
+}
