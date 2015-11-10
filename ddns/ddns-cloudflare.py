c841a8f04bdb2d962be788c25c79523e32977673
Andrei Vacariu <andrei@avacariu.me>
2015-11-10 12:21:06 -0800

    Add IPv6 support to cloudflare ddns script

--- ddns/ddns-cloudflare.py
+++ ddns/ddns-cloudflare.py
@@ -19,20 +19,23 @@ import sys
 # get the old IP address
 dns_name = "%s.%s" % (CF_REC_NAME, CF_ZONE)
 try:
-    old_ip = subprocess.check_output(["dig +short %s" % dns_name], shell=True).strip().decode()
+    old_ip4 = subprocess.check_output(["dig +short %s A" % dns_name], shell=True).strip().decode()
+    old_ip6 = subprocess.check_output(["dig +short %s AAAA" % dns_name], shell=True).strip().decode()
 except Exception as e:
     print("Failed to get old IP.", e)
     sys.exit(1)
 
 # get the new IP address
 try:
-    current_ip = subprocess.check_output(["dig +short myip.opendns.com @resolver1.opendns.com"], shell=True).strip().decode()
+    current_ip4 = subprocess.check_output(["dig +short myip.opendns.com @resolver1.opendns.com"], shell=True).strip().decode()
+    current_ip6 = subprocess.check_output(["curl -6 utils.80x24.ca/my_ip"], shell=True).strip().decode()
 except Exception as e:
     print("Failed to get IP.", e)
     sys.exit(1)
 
 # check that they're different
-if old_ip == current_ip:
+if old_ip4 == current_ip4 and old_ip6 == current_ip6:
+    print("IPs haven't changed")
     sys.exit(0)
 
 # the IP has changed, so let's update the record
@@ -41,11 +44,13 @@ rec_id = None
 
 for rec in cf.rec_load_all(CF_ZONE):
     if rec['display_name'] == CF_REC_NAME and rec['type'] == 'A':
-        rec_id = rec['rec_id']
-        break
+        rec_id4 = rec['rec_id']
+    elif rec['display_name'] == CF_REC_NAME and rec['type'] == 'AAAA':
+        rec_id6 = rec['rec_id']
 
-if rec_id is None:
+if rec_id4 is None:
     print("Couldn't find the record.")
     sys.exit(2)
 
-cf.rec_edit(CF_ZONE, 'A', int(rec_id), CF_REC_NAME, current_ip)
+cf.rec_edit(CF_ZONE, 'A', int(rec_id4), CF_REC_NAME, current_ip4)
+cf.rec_edit(CF_ZONE, 'AAAA', int(rec_id6), CF_REC_NAME, current_ip6)
