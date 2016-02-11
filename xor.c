f2ca56142b07e7c79bd8815a870cc906e54cb90f
Andrei Vacariu <andrei@avacariu.me>
2016-02-10 19:30:15 -0800

    Wrote a basic xor encryption program

---
+++ xor.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        printf("xor key INPUTFILE OUTPUTFILE\n");
+        return 1;
+    }
+
+    char *key = argv[1];
+    size_t key_len = strlen(key);
+    char *filename = argv[2];
+    char *outfilename = argv[3];
+
+    FILE *infile = fopen(filename, "rb");
+    FILE *outfile = fopen(outfilename, "wb");
+
+    int64_t counter = 0;
+
+    while (true) {
+        int next_char = fgetc(infile);
+        if (next_char == EOF) {
+            break;
+        }
+
+        fputc(next_char ^ (key[counter % key_len]), outfile);
+    }
+
+    fclose(infile);
+    fclose(outfile);
+}
